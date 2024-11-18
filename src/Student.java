7e6498e4554e9e856e52d1e56bb0f3ca5c119497
Trush564 <161017912+Trush564@users.noreply.github.com>
2024-11-18 20:57:41 +0200

    first commit

--- src/Student.java
+++ src/Student.java
@@ -1,2 +1,9 @@
-package PACKAGE_NAME;public class Student {
+public class Student {
+    String name;
+    String sex;
+
+    public Student(java.lang.String name, java.lang.String sex) {
+        this.name = name;
+        this.sex = sex;
+    }
 }
--- src/StudentComparator.java
+++ src/StudentComparator.java
@@ -1,2 +1,10 @@
-package PACKAGE_NAME;public class StudentComparator {
+import java.util.Comparator;
+public class StudentComparator implements  Comparator<Student>{
+    public int compare (Student s1, Student s2){
+        int sex = s1.sex.compareTo(s2.sex);
+        if(sex!=0){
+            return sex;
+        }
+        return s1.name.compareTo(s2.name);
+    }
 }
